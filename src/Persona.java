d58abd8b1bc03161c95a461f056e3edf9acd53ab
Cesa <pcesarmata@gmail.com>
2021-02-22 21:15:17 -0600

    init commit

---
+++ src/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+
+    // Atributos de la clase
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+
+    // Metodo de la clase
+    public void desplegarNombre() {
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellidoPaterno = " + apellidoPaterno);
+        System.out.println("apellidoMaterno = " + apellidoMaterno);
+    }
+}
